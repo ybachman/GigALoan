b8b20c2768333148ee947c17ac12d93d350df108
rlister <Ryan.Lister@aarons.com>
2016-04-05 19:25:28 -0400

    Wrote a bunch of Services

--- GigALoan_Model/DTO_CORE_Student.cs
+++ GigALoan_Model/DTO_CORE_Student.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace GigALoan_Model
 {
@@ -16,8 +17,20 @@ namespace GigALoan_Model
         public string FirstName { get; set; }
         [DataMember]
         public string LastName { get; set; }
-        [DataMember]
+        
         public DateTime DateJoined { get; set; }
+        [DataMember(Name = "DateJoined")]
+        private string CreationDateForSerialization { get; set; }
+        [OnSerializing]
+        void OnSerializing(StreamingContext context)
+        {
+            this.CreationDateForSerialization = JsonConvert.SerializeObject(this.DateJoined).Replace('"', ' ').Trim();
+        }
+        [OnDeserialized]
+        void OnDeserialized(StreamingContext context)
+        {
+            this.DateJoined = DateTime.Parse(this.CreationDateForSerialization);
+        }
         [DataMember]
         public string Email { get; set; }
         [DataMember]
